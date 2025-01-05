5126f1143208fdd0d63dfd96e4586fa8e7fa805e
shurona <khc4669@naver.com>
2025-01-05 21:22:39 +0900

    feat: 로그인 세션 길이 조정

--- src/main/java/shurona/wordfinder/user/controller/LoginController.java
+++ src/main/java/shurona/wordfinder/user/controller/LoginController.java
@@ -2,27 +2,27 @@ package shurona.wordfinder.user.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
-import org.apache.tomcat.util.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import shurona.wordfinder.common.PasswdToHash;
-import shurona.wordfinder.user.domain.User;
 import shurona.wordfinder.user.common.SessionConst;
 import shurona.wordfinder.user.controller.dto.LoginForm;
+import shurona.wordfinder.user.domain.User;
 import shurona.wordfinder.user.service.UserService;
 import shurona.wordfinder.user.session.UserSession;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
-
 @Controller
 public class LoginController {
+
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     private final UserService userService;
@@ -40,19 +40,23 @@ public class LoginController {
 
     @PostMapping("login")
     public String loginProcess(
-            @Validated @ModelAttribute("loginForm") LoginForm form,
-            BindingResult bindingResult,
-            HttpServletRequest request,
-            @RequestParam(value = "redirectURL", defaultValue = "/") String redirectURL
+        @Validated @ModelAttribute("loginForm") LoginForm form,
+        BindingResult bindingResult,
+        HttpServletRequest request,
+        @RequestParam(value = "redirectURL", defaultValue = "/") String redirectURL
     ) {
-        User loginUser = this.userService.login(form.getLoginId(), PasswdToHash.doProcess(form.getPassword()));
+        User loginUser = this.userService.login(form.getLoginId(),
+            PasswdToHash.doProcess(form.getPassword()));
 
         if (loginUser == null) {
             bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
             return "user/login/loginForm";
         }
         HttpSession session = request.getSession();
-        UserSession userSession = UserSession.createUserSession(loginUser.getId(), loginUser.getRole());
+        UserSession userSession = UserSession.createUserSession(loginUser.getId(),
+            loginUser.getRole());
+        //TODO: 로그인 시간 외부에서 주입하도록 수정
+        session.setMaxInactiveInterval(60 * 60 * 2);
         session.setAttribute(SessionConst.LOGIN_USER, userSession);
 
         return "redirect:" + redirectURL;
