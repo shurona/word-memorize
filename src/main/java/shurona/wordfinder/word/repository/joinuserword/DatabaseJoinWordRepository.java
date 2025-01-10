acbb5a962c750942fc47fe3ca74b30cbb7fe026b
shurona <khc4669@naver.com>
2025-01-10 17:47:49 +0900

    refactor: 코드 포매터 적용

--- src/main/java/shurona/wordfinder/word/repository/joinuserword/DatabaseJoinWordRepository.java
+++ src/main/java/shurona/wordfinder/word/repository/joinuserword/DatabaseJoinWordRepository.java
@@ -2,16 +2,15 @@ package shurona.wordfinder.word.repository.joinuserword;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import java.util.List;
 import org.springframework.stereotype.Repository;
 import shurona.wordfinder.user.domain.User;
 import shurona.wordfinder.word.domain.JoinWordUser;
 import shurona.wordfinder.word.domain.Word;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 @Repository
 public class DatabaseJoinWordRepository implements JoinWordRepository {
+
     @PersistenceContext
     EntityManager em;
 
@@ -27,19 +26,19 @@ public class DatabaseJoinWordRepository implements JoinWordRepository {
     public List<JoinWordUser> findListByIds(List<String> ids) {
         String query = "select jwu from JoinWordUser as jwu where jwu.uid in :ids";
         return this.em.createQuery(query, JoinWordUser.class)
-                .setParameter("ids", ids)
-                .getResultList();
+            .setParameter("ids", ids)
+            .getResultList();
     }
 
     @Override
     public JoinWordUser findByUserWithWord(Long userId, String wordUid) {
         // visible 검사는 안함
         String query = "select jwu from JoinWordUser as jwu join fetch jwu.word " +
-                "where jwu.user.id = :userId and jwu.word.id = :wordId";
+            "where jwu.user.id = :userId and jwu.word.id = :wordId";
         List<JoinWordUser> joinWordUserList = this.em.createQuery(query, JoinWordUser.class)
-                .setParameter("userId", userId)
-                .setParameter("wordId", wordUid)
-                .getResultList();
+            .setParameter("userId", userId)
+            .setParameter("wordId", wordUid)
+            .getResultList();
 
         if (joinWordUserList.isEmpty()) {
             return null;
@@ -52,8 +51,8 @@ public class DatabaseJoinWordRepository implements JoinWordRepository {
         String query = "select jwu from JoinWordUser as jwu where jwu.user.id = :userId and jwu.visible = true";
 
         List<JoinWordUser> joinWordUserList = this.em.createQuery(query, JoinWordUser.class)
-                .setParameter("userId", userId)
-                .getResultList();
+            .setParameter("userId", userId)
+            .getResultList();
 
         return joinWordUserList.toArray(JoinWordUser[]::new);
     }
@@ -61,20 +60,23 @@ public class DatabaseJoinWordRepository implements JoinWordRepository {
     @Override
     public JoinWordUser[] joinWordList() {
         String query = "select jwu from JoinWordUser as jwu where jwu.visible = true";
-        List<JoinWordUser> resultList = this.em.createQuery(query, JoinWordUser.class).getResultList();
+        List<JoinWordUser> resultList = this.em.createQuery(query, JoinWordUser.class)
+            .getResultList();
         return resultList.toArray(JoinWordUser[]::new);
     }
 
     @Override
     public JoinWordUser[] pickListForQuiz(Long userId, int offset, int limit) {
-        String query = "select jwu from JoinWordUser as jwu join fetch jwu.word where jwu.user.id = :userId and jwu.visible = true " +
+        String query =
+            "select jwu from JoinWordUser as jwu join fetch jwu.word where jwu.user.id = :userId and jwu.visible = true "
+                +
                 "order by jwu.lastSelectedQuiz desc";
 
         List<JoinWordUser> joinWordUserList = this.em.createQuery(query, JoinWordUser.class)
-                .setParameter("userId", userId)
-                .setFirstResult(offset)
-                .setMaxResults(limit)
-                .getResultList();
+            .setParameter("userId", userId)
+            .setFirstResult(offset)
+            .setMaxResults(limit)
+            .getResultList();
 
         return joinWordUserList.toArray(JoinWordUser[]::new);
     }
@@ -82,31 +84,32 @@ public class DatabaseJoinWordRepository implements JoinWordRepository {
     @Override
     public JoinWordUser[] pickRandomForQuiz(Long userId, int offset, int limit) {
         String query = "select jwu from JoinWordUser as jwu " +
-                "where jwu.id in " +
-                "(select j.id from JoinWordUser as j where j.user.id = :userId and j.visible = true " +
-                "order by j.lastSelectedQuiz desc offset :offset)"
-                + "order by random()";
+            "where jwu.id in " +
+            "(select j.id from JoinWordUser as j where j.user.id = :userId and j.visible = true " +
+            "order by j.lastSelectedQuiz desc offset :offset)"
+            + "order by random()";
 
 //        String query = "select jwu from JoinWordUser as jwu where jwu.user.id = :userId order by jwu.updatedAt";
 
         List<JoinWordUser> joinWordUserList = this.em.createQuery(query, JoinWordUser.class)
-                .setParameter("userId", userId)
-                .setParameter("offset", offset)
-                .setMaxResults(limit)
-                .getResultList();
+            .setParameter("userId", userId)
+            .setParameter("offset", offset)
+            .setMaxResults(limit)
+            .getResultList();
         return joinWordUserList.toArray(JoinWordUser[]::new);
     }
 
     public int countWordUserByUserId(Long userId, boolean excludeHide) {
         String query = "select count(jwu.id) from JoinWordUser as jwu " +
-                "where jwu.user.id = :userId ";
+            "where jwu.user.id = :userId ";
 
         // 만약 hide를 포함하지 않으면
         if (excludeHide) {
             query += "and jwu.visible = true";
         }
 
-        List<Long> resultList = this.em.createQuery(query, Long.class).setParameter("userId", userId).getResultList();
+        List<Long> resultList = this.em.createQuery(query, Long.class)
+            .setParameter("userId", userId).getResultList();
         return resultList.get(0).intValue();
     }
 
