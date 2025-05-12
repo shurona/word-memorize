a0b7e1f115cccb42a70dc40d5eaabe24255920fc
shurona <khc4669@naver.com>
2025-05-12 14:45:50 +0900

    feat: 헤더 접근 제어 처리를 위한 테스트

--- src/main/java/shurona/wordfinder/custom/controller/LineHookController.java
+++ src/main/java/shurona/wordfinder/custom/controller/LineHookController.java
@@ -1,6 +1,11 @@
 package shurona.wordfinder.custom.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,35 +13,56 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-import shurona.wordfinder.custom.dto.line.WebHookRequest;
 
 @RestController
 public class LineHookController {
 
+    private final Environment environment;
+
+    @Autowired
+    public LineHookController(Environment environment) {
+        this.environment = environment;
+    }
 
     @PostMapping
     public ResponseEntity<?> checkUrl(
         @RequestHeader("x-line-signature") String header,
-        @RequestBody WebHookRequest requestBody
+        @RequestBody String requestBody
     ) {
 
         System.out.println("?? : " + header);
 
+        try {
+            // header validation
+            String channelSecret =
+                this.environment.getProperty("line.secret-key");// Channel secret string
+
+            SecretKeySpec key = new SecretKeySpec(channelSecret.getBytes(), "HmacSHA256");
+            Mac mac = Mac.getInstance("HmacSHA256");
+            mac.init(key);
+            byte[] source = requestBody.getBytes("UTF-8");
+            String signature = Base64.encodeBase64String(mac.doFinal(source));
+
+            System.out.println(signature + " : " + signature.equals(header));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+
         ObjectMapper objectMapper = new ObjectMapper();
 
-        try {
-            // 객체를 JSON 문자열로 변환 (Node.js의 JSON.stringify와 동일)
-            String json = objectMapper.writeValueAsString(requestBody);
-
-            System.out.println("사이즈는 : " + requestBody.events().size());
-
-            System.out.println(json); // 혹은 로그로 출력
-
-            System.out.println(
-                objectMapper.writeValueAsString(requestBody.events().get(0).message().get()));
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+//        try {
+//            // 객체를 JSON 문자열로 변환 (Node.js의 JSON.stringify와 동일)
+//            String json = objectMapper.writeValueAsString(requestBody);
+//
+//            System.out.println("사이즈는 : " + requestBody.events().size());
+//
+//            System.out.println(json); // 혹은 로그로 출력
+//
+//            System.out.println(
+//                objectMapper.writeValueAsString(requestBody.events().get(0).message().get()));
+//        } catch (Exception e) {
+//            System.out.println(e.getMessage());
+//        }
 
         return ResponseEntity.ok().build();
     }
