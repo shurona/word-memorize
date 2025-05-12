4a16a11333c313a7cc4950559868f0b58ce4222e
shurona <khc4669@naver.com>
2025-05-12 14:52:00 +0900

    feat: 접근 제어 최종 검증 테스트

--- src/main/java/shurona/wordfinder/custom/controller/LineHookController.java
+++ src/main/java/shurona/wordfinder/custom/controller/LineHookController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import shurona.wordfinder.custom.dto.line.WebHookRequestDto;
 
 @RestController
 public class LineHookController {
@@ -27,7 +28,7 @@ public class LineHookController {
     @PostMapping
     public ResponseEntity<?> checkUrl(
         @RequestHeader("x-line-signature") String header,
-        @RequestBody String requestBody
+        @RequestBody String requestBodyOrigin
     ) {
 
         System.out.println("?? : " + header);
@@ -40,7 +41,7 @@ public class LineHookController {
             SecretKeySpec key = new SecretKeySpec(channelSecret.getBytes(), "HmacSHA256");
             Mac mac = Mac.getInstance("HmacSHA256");
             mac.init(key);
-            byte[] source = requestBody.getBytes("UTF-8");
+            byte[] source = requestBodyOrigin.getBytes("UTF-8");
             String signature = Base64.encodeBase64String(mac.doFinal(source));
 
             System.out.println(signature + " : " + signature.equals(header));
@@ -50,19 +51,21 @@ public class LineHookController {
 
         ObjectMapper objectMapper = new ObjectMapper();
 
-//        try {
-//            // 객체를 JSON 문자열로 변환 (Node.js의 JSON.stringify와 동일)
-//            String json = objectMapper.writeValueAsString(requestBody);
-//
-//            System.out.println("사이즈는 : " + requestBody.events().size());
-//
-//            System.out.println(json); // 혹은 로그로 출력
-//
-//            System.out.println(
-//                objectMapper.writeValueAsString(requestBody.events().get(0).message().get()));
-//        } catch (Exception e) {
-//            System.out.println(e.getMessage());
-//        }
+        try {
+            // 객체를 JSON 문자열로 변환 (Node.js의 JSON.stringify와 동일)
+            ObjectMapper mapper = new ObjectMapper();
+            WebHookRequestDto requestBody = mapper.readValue(requestBodyOrigin,
+                WebHookRequestDto.class);
+
+            System.out.println("사이즈는 : " + requestBody.events().size());
+
+            System.out.println(requestBodyOrigin); // 혹은 로그로 출력
+
+            System.out.println(
+                objectMapper.writeValueAsString(requestBody.events().get(0).message().get()));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
         return ResponseEntity.ok().build();
     }
--- src/main/java/shurona/wordfinder/custom/dto/line/WebHookRequestDto.java
+++ src/main/java/shurona/wordfinder/custom/dto/line/WebHookRequestDto.java
@@ -3,7 +3,7 @@ package shurona.wordfinder.custom.dto.line;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
-public record WebHookRequest(
+public record WebHookRequestDto(
     @JsonProperty("destination") String destination,
     @JsonProperty("events") List<LineEvent> events
 ) {
